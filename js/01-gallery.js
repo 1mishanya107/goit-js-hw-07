d7534c4f0bb0ce36dc1c444afdade7135f0fb40d
1mishanya107 <1mishanya107@bk.ru>
2021-11-15 16:17:15 +0400

    1 ЗАДАНИЕ
    1 задание

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCards(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+
+function createGalleryCards(cards) {
+    return cards
+        .map(({ preview, original, description }) => {
+            return `
+                <div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        />
+                    </a>
+                </div>
+                `
+        })
+        .join('');
+
+}
+
+
+document.querySelector('.gallery').onclick = (e) => {
+    e.preventDefault();
+    // console.log(e.currentTarget)
+
+
+    if (e.target.nodeName !== "IMG") {
+        return
+    }
+    else {
+	basicLightbox.create(`
+		<img width=auto src="${e.target.dataset.source}">
+	`).show()
+}
+}
